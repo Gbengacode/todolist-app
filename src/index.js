f7074f38bdabb56fbda96dd2b87c03da0ff30984
emmanuel <gbengacode>
2021-11-30 23:28:59 +0100

    Fix linters error

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import './css/main.css';
 import Items from './data.js';
-import template from './template.js';
+// import template from './template.js';
 
-const container = document.querySelector('.container');
-const subContainer = document.createElement('div');
-const form = document.createElement('form');
-const input = document.createElement('input');
-const ul = document.createElement('ul');
-input.setAttribute('type', 'text');
-input.setAttribute('placeholder', 'Add to your list');
-form.appendChild(input);
-subContainer.className = 'sub-container';
-const h1 = document.createElement('h1');
-h1.textContent = "Today's to do";
-const icon1 = document.createElement('i');
-icon1.className = 'fas';
-icon1.classList.add('fa-sync-alt');
-subContainer.appendChild(h1);
-subContainer.appendChild(icon1);
-container.appendChild(subContainer);
-container.appendChild(form);
-
-template(Items, container, ul);
+// const container = document.querySelector('.container');
+// const subContainer = document.createElement('div');
+// const form = document.createElement('form');
+// const input = document.createElement('input');
+// const ul = document.createElement('ul');
+// input.setAttribute('type', 'text');
+// input.setAttribute('placeholder', 'Add to your list');
+// form.appendChild(input);
+// subContainer.className = 'sub-container';
+// const h1 = document.createElement('h1');
+// h1.textContent = "Today's to do";
+// const icon1 = document.createElement('i');
+// icon1.className = 'fas';
+// icon1.classList.add('fa-sync-alt');
+// subContainer.appendChild(h1);
+// subContainer.appendChild(icon1);
+// container.appendChild(subContainer);
+// container.appendChild(form);
+// console.log(container)
+//template(Items, container, ul);
--- src/template.js
+++ src/template.js
@@ -1,11 +1,9 @@
 export default (Items, container, ul) => {
   Items.forEach((item) => {
-    ul.innerHTML += `<li><span><i class="far fa-square"></i>${item.description} </span> <i class="fa fa-ellipsis-v" aria-hidden="true"></i></li> `;
+    ul.innerHTML += `<li><span><i class="far fa-square"></i>${item.description} </span> <i class="fa fa-ellipsis-v" aria-hidden="true"></i></li> <hr/>`;
   });
 
-  
   container.appendChild(ul);
-  
   container.innerHTML += `
   
   <div class="clear"><p>Clear all completed</p></div>
