005202267a0ad11cf8f50014df09607b6a27a824
emmanuel <gbengacode>
2021-11-30 22:44:47 +0100

    Add color

--- src/template.js
+++ src/template.js
@@ -1,9 +1,11 @@
 export default (Items, container, ul) => {
   Items.forEach((item) => {
-    ul.innerHTML += `<li><span><i class="far fa-square"></i>${item.description} </span> <i class="fa fa-ellipsis-v" aria-hidden="true"></i></li> <hr/>`;
+    ul.innerHTML += `<li><span><i class="far fa-square"></i>${item.description} </span> <i class="fa fa-ellipsis-v" aria-hidden="true"></i></li> `;
   });
 
+  
   container.appendChild(ul);
+  
   container.innerHTML += `
   
   <div class="clear"><p>Clear all completed</p></div>
